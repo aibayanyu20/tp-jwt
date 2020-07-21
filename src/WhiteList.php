c61da7645a54cabf722edfda67349e73578dce34
aibayanyu <aibayanyu@qq.com>
2020-07-21 22:17:04 +0800

    提交修改文件

--- src/WhiteList.php
+++ src/WhiteList.php
@@ -17,6 +17,12 @@ class WhiteList
         $this->list = cache("aibayanyu_jwt_white_list");
     }
 
+    /**
+     * 将当前的token加入至白名单
+     * @param $token
+     * @author: 朱正健
+     * Time: 2020/7/21 22:01
+     */
     public function join($token){
         // 将token写入至白名单
         array_push($this->list,$token);
